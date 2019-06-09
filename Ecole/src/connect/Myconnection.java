d8deb3391b16d3201c632e1516649302feb1b8e2
QuentinRP <47528421+QuentinRP@users.noreply.github.com>
2019-06-09 23:51:43 +0200

    Add files via upload

--- Ecole/src/connect/Myconnection.java
+++ Ecole/src/connect/Myconnection.java
@@ -3,6 +3,7 @@ package connect;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -15,7 +16,26 @@ import java.sql.DriverManager;
  * @author Pierre-Louis
  */
 public class Myconnection {
+    private static Connection con;
+
+	public Myconnection(String user, String password) throws SQLException
+	{
+            try {
+		Class.forName("com.mysql.jdbc.Driver");
+		con = DriverManager.getConnection("jdbc:mysql://localhost/donnee_ecole", user, password); /*on récupère l'username et le wordpass permettant une connexion à la base de donnée*/
+	} catch (ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+            
+        }
+
+	public static Connection getConnection(){
+		return con;
+
+	}
+}
     
+    /*
     public static Connection getConnection()
     {
         Connection con = null;
@@ -29,5 +49,6 @@ public class Myconnection {
         
         return con;
         
-    }    
-}
+    } */   
+
+
